127bbda73be148277544fa249f3a6a1b80ae3492
z-anshun <57032282+z-anshun@users.noreply.github.com>
2020-03-08 22:02:08 +0800

    Update jwt.go

--- first_work/jwt/jwt.go
+++ first_work/jwt/jwt.go
@@ -12,15 +12,21 @@ import (
 	"time"
 )
 
-func NewHeader() Header {
-	return Header{
+
+
+func NewJwt() Jwt  {
+	return Jwt{
+		Header{
 		Api: "HS256",
 		Typ: "JWT",
+	},
+	PlayLoad{},
+	"",
 	}
 }
 
-func CreatJwt(name string, id int) string {
-	h := NewHeader()
+func (j *Jwt)CreatJwt(name string, id int) string {
+
 	p := PlayLoad{
 		UserName: name,
 		Id:       id,
@@ -28,7 +34,7 @@ func CreatJwt(name string, id int) string {
 		Iat:      strconv.FormatInt(time.Now().Unix(), 10),
 		Exp:      strconv.FormatInt(time.Now().Add(time.Hour*20).Unix(), 10),
 	}
-	header, err := json.Marshal(&h) //转化为json
+	header, err := json.Marshal(&j.Header) //转化为json
 	if err != nil {
 		log.Fatal("json err:", err)
 	}
@@ -46,47 +52,46 @@ func CreatJwt(name string, id int) string {
 	mac.Write([]byte(str))                   //写入
 	s := mac.Sum(nil)
 
-	signature := base64.StdEncoding.EncodeToString(s)
+	j.Signature = base64.StdEncoding.EncodeToString(s)
 	//header+"."+playload+"."+signature
-	token := str + "." + signature
+	token := str + "." + j.Signature
 
 	return token
 
-
 }
-func CheckToken(token string) (uid int, name string, err error) {
+func (j *Jwt)CheckToken(token string) (uid int, name string, err error) {
 	arr := strings.Split(token, ".")
-	if len(arr)!=3{
-		return  0,"",errors.New("token error")
+	if len(arr) != 3 {
+		return 0, "", errors.New("token error")
 	}
 	_, err = base64.StdEncoding.DecodeString(arr[0])
 	if err != nil {
 		return 0, "", err
 	}
-	p,err:=base64.StdEncoding.DecodeString(arr[1])
-	if err!=nil{
-		return  0,"",err
+	p, err := base64.StdEncoding.DecodeString(arr[1])
+	if err != nil {
+		return 0, "", err
 	}
-	s,err:=base64.StdEncoding.DecodeString(arr[2])
-	if err!=nil{
-		return  0,"",err
+	s, err := base64.StdEncoding.DecodeString(arr[2])
+	if err != nil {
+		return 0, "", err
 	}
-	str:=arr[0]+"."+arr[1]
+	str := arr[0] + "." + arr[1]
 
 	key := "first"
 	mac := hmac.New(sha256.New, []byte(key)) //创建一个hash
 	mac.Write([]byte(str))                   //写入
 	s1 := mac.Sum(nil)
 
-	if !hmac.Equal(s,s1){
-		return  0,"",errors.New("check error")
+	if !hmac.Equal(s, s1) {
+		return 0, "", errors.New("check error")
 	}
 
 	var playload PlayLoad
 	//json化
-	if err=json.Unmarshal(p,&playload) ;err!=nil{
+	if err = json.Unmarshal(p, &playload); err != nil {
 		log.Fatal("json error")
 	}
 
-	return playload.Id,playload.UserName,nil
+	return playload.Id, playload.UserName, nil
 }
